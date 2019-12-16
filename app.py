db9f9b22f85ee40d35a37e7ee7844100a54f20ee
Vladislav Novak <novak.vlad97@mail.ru>
2019-12-16 11:05:04 +0300

    Try fix again...

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pymongo
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
-app = Flask(__name__, static_url_path='', static_folder='client/build')
+app = Flask(__name__, template_folder="client/build", static_folder="client/build/static")
 
 
 client = pymongo.MongoClient("mongodb+srv://Vlad123:Vlad123@devconnector-e46b3.mongodb.net/test?retryWrites=true&w=majority")
@@ -11,9 +11,11 @@ users_collection = db_apartPlanner["users"]
 # for item in users_collection.find():
 #     return item
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    """ This is a catch all that is required for react-router """
+    return render_template('index.html')
 
 @app.route('/api/users')
 def index():
